50cb6587d8d7231d90ed8236d987875b699d457b
OnKi_Kim <77190699+monki4746@users.noreply.github.com>
2023-08-21 23:31:40 +0900

    Add files via upload

---
+++ JavaWorkspace/src/chap_01/_03_Variables.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _03_Variables {
+    public static void main(String[] args) {
+        String name = "온기";  //문자열 변수
+        int hour = 15;  //정수형 변수
+
+        System.out.println(name + "님, 배송이 시작되었습니다." + hour + "시에 방문 예정입니다.");
+        System.out.println(name + "님, 배송이 완료되었습니다.");
+
+        double score
+    }
+}
