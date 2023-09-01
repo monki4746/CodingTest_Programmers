23a5f46e355ff33ab625520e08ef8f913ac4a3cf
OnKi_Kim <77190699+monki4746@users.noreply.github.com>
2023-09-01 13:41:47 +0900

    Create 120807.java

---
+++ Level0/120807.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = (num1 == num2) ? 1:-1;
+            
+        // if (num1==num2) {
+        //     answer =1;
+        // }
+        // else{
+        //     answer=-1;
+        // }
+        return answer;
+    }
+}
